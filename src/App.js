39edcb2ea312266b424e33c8fb2674fb1f8bcabf
SHAHID KHAN <shahidmerdeveloper@gmail.com>
2021-06-07 00:07:46 +0500

    urdu - arabic  font family

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./sik-css-fw/13.theme/my-theme.scss";
 function App() {
-  return <div>REACT BOILER PLATE</div>;
+  return (
+    <>
+      <div-a> بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ</div-a>
+      <div>REACT BOILER PLATE</div>
+      <div-u>شاہد اقبال خان</div-u>
+    </>
+  );
 }
 
 export default App;
