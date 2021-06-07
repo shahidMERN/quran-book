bfb4c5bda96a1049e3ac5a32c738a793abeb101a
SHAHID KHAN <shahidmerdeveloper@gmail.com>
2021-06-07 18:45:48 +0500

    margin padding

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./khan-css/13.theme/Theme.scss";
 function App() {
   return (
-    <>
-      <div class="container-full">
-        <div class="arabic"> بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ</div>
-        <div>REACT BOILER PLATE</div>
-        <div class="urdu">شاہد اقبال خان</div>
-      </div>
-    </>
+    <div class="container">
+      <div class="arabic-text"> بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ</div>
+      <div>REACT BOILER PLATE</div>
+      <div class="urdu-text">شاہد اقبال خان</div>
+    </div>
   );
 }
 
